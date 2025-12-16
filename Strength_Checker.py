937601449939801ff13da1fc177d89b7039517bf
Ethyn <ethynmalin@gmail.com>
2025-12-15 22:38:42 -0700

    This version is a variable way of checking strength. The shorter a password, the more strict it is on how equal character counts should be. More entropy is rewarded with shorter values, yet it still wont rank as high as a long password with lower entropy.

--- Strength_Checker.py
+++ Strength_Checker.py
@@ -1,18 +1,21 @@
+from math import sqrt
+
 pswd = input('Enter password: ')
 
-char_amt = 0
+
 int_amt = 0
 cap = 0
 low = 0
 sym = 0
+
+password_strength = 0
 
 for char in pswd:
     try:
         int(char)
         int_amt += 1
     except ValueError:
-        if char.isalpha():
-            char_amt += 1
+        pass
     if char.isupper():
         cap += 1
     elif char.islower():
@@ -21,6 +24,33 @@ for char in pswd:
         sym += 1
 
 
-print(char_amt, int_amt, cap, low, sym)
+print(int_amt, cap, low, sym)
+
 passwd_len = len(pswd)
-val = passwd_len / 5
+if passwd_len < 8:
+    change_var = 2
+elif 8 <= passwd_len < 16:
+    change_var = 6
+elif 16 <= passwd_len:
+    change_var = 10
+
+equal_chars = passwd_len / 4  # how many instances of a thing there should be
+margin_OE = sqrt(equal_chars) # Parameter of error
+
+def check_value(parameter1, parameter2):
+    global password_strength
+    difference = abs(parameter1 - parameter2)
+    print(f'difference is {difference}')
+    print(f'marginOE is {margin_OE}')
+    if difference <= margin_OE:
+        password_strength += change_var
+    
+print(equal_chars)
+
+check_value(equal_chars, int_amt)
+check_value(equal_chars, cap)
+check_value(equal_chars, low)
+check_value(equal_chars, sym)
+
+
+print(password_strength)
