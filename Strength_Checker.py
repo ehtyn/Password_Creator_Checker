68cb09bde3227689f09155ac9840957ce4748615
Ethyn <ethynmalin@gmail.com>
2025-12-16 08:51:24 -0700

    Added seen_chars, which checks if a character in a password has been seen before. Lowers the overall score of the password if it is. Also added colorama for readability of output text, and a score cap of 100.

--- Strength_Checker.py
+++ Strength_Checker.py
@@ -1,4 +1,6 @@
 from math import sqrt
+from colorama import Fore, Style, init
+init(autoreset = True)
 
 pswd = input('Enter password: ')
 
@@ -24,7 +26,8 @@ for char in pswd:
         sym += 1
 
 
-print(int_amt, cap, low, sym)
+
+
 
 passwd_len = len(pswd)
 if passwd_len < 8:
@@ -40,12 +43,9 @@ margin_OE = sqrt(equal_chars) # Parameter of error
 def check_value(parameter1, parameter2):
     global password_strength
     difference = abs(parameter1 - parameter2)
-    print(f'difference is {difference}')
-    print(f'marginOE is {margin_OE}')
-    if difference <= margin_OE:
+    if difference < margin_OE:
         password_strength += change_var
     
-print(equal_chars)
 
 check_value(equal_chars, int_amt)
 check_value(equal_chars, cap)
@@ -53,4 +53,49 @@ check_value(equal_chars, low)
 check_value(equal_chars, sym)
 
 
-print(password_strength)
+
+
+seen_chars = {} # Creates an empty dictionary of seen characters
+
+for char in pswd:
+    if char not in seen_chars: # Checks if the character isnt in seen chars, than if it isnt it adds it with counter of 1
+        seen_chars[char] = 1
+    else:
+        seen_chars[char] += 1 # Adds 1 to the seen counter if counter exists
+
+acceptable_repeats = sqrt(passwd_len)
+
+
+for value in seen_chars.values():
+    if value > acceptable_repeats:
+        password_strength -= change_var
+    else: 
+        password_strength += change_var
+
+
+max_score = 250
+
+if password_strength >= max_score:
+    password_strength = 100
+else:
+    pass
+
+final_score = password_strength
+
+final_score = min(final_score, 100)
+
+def print_color():
+    if 20 >= final_score:
+        color = Fore.RED
+    elif 40 >= final_score > 20:
+        color = Fore.LIGHTRED_EX
+    elif 60 >= final_score > 40:
+        color = Fore.YELLOW
+    elif 80 >= final_score > 60:
+        color = Fore.LIGHTGREEN_EX
+    elif 100 >= final_score > 80:
+        color = Fore.GREEN
+
+    return color
+
+print(print_color() + f'Your password ranked a score of {final_score}%. ')
