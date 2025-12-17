cb5c3ce48dc0fb164f354b238c726ebc688c9440
Ethyn <ethynmalin@gmail.com>
2025-12-16 22:38:29 -0700

    Changed data structure to list of dictionaries. This will make graphing better for future update.

--- tester.py
+++ tester.py
@@ -3,6 +3,7 @@ import characters_list
 import random
 import json
 from pathlib import Path
+import plotly.express as px
 
 parent_folder = Path(__file__).parent
 json_file = parent_folder / "tested_passwords.json"
@@ -17,7 +18,7 @@ with open(json_file, 'w') as f:
 def create_password():
     random_password = []
     password = ""
-    random_length = random.randint(1, 20)
+    random_length = random.randint(1, 20) 
     while len(random_password) < random_length:
         random_selection = characters_list.select_random_char()
         random_password.append(random_selection)
@@ -27,14 +28,14 @@ def create_password():
 data = []
 
 blank_return = Strength_Checker.main("")
-data.append(f'Blank password "" returned a score of {blank_return}')
+data.append({'Password': "", "Score": blank_return})
 
 for i in range(1000):
     passkey_to_test = None
     result = None
     passkey_to_test = create_password()
     result = Strength_Checker.main(passkey_to_test)
-    data.append(f'The password {passkey_to_test} returned a score of {result}%. ')
+    data.append({'Password': passkey_to_test, "Score": result})
 
 with open(json_file, 'w') as f:
     json.dump(data, f, indent = 2)
