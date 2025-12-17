1815bb6490359f5676265dfe3df87433381354eb
Ethyn <ethynmalin@gmail.com>
2025-12-16 22:50:26 -0700

    Added main function to tester.py for future modularity.

--- tester.py
+++ tester.py
@@ -3,40 +3,46 @@ import characters_list
 import random
 import json
 from pathlib import Path
-import plotly.express as px
+
 
 parent_folder = Path(__file__).parent
 json_file = parent_folder / "tested_passwords.json"
 
+def main():
+
+    with open(json_file, 'w') as f:
+        json.dump('', f)
 
 
-with open(json_file, 'w') as f:
-    json.dump('', f)
 
+    def create_password():
+        random_password = []
+        password = ""
+        random_length = random.randint(1, 20) 
+        while len(random_password) < random_length:
+            random_selection = characters_list.select_random_char()
+            random_password.append(random_selection)
+            password = "".join(random_password)
+        return password
 
+    data = []
 
-def create_password():
-    random_password = []
-    password = ""
-    random_length = random.randint(1, 20) 
-    while len(random_password) < random_length:
-        random_selection = characters_list.select_random_char()
-        random_password.append(random_selection)
-        password = "".join(random_password)
-    return password
+    blank_return = Strength_Checker.main("")
+    data.append({'Password': "", "Score": blank_return})
 
-data = []
+    for i in range(1000):
+        passkey_to_test = None
+        result = None
+        passkey_to_test = create_password()
+        result = Strength_Checker.main(passkey_to_test)
+        data.append({'Password': passkey_to_test, "Score": result})
 
-blank_return = Strength_Checker.main("")
-data.append({'Password': "", "Score": blank_return})
+    with open(json_file, 'w') as f:
+        json.dump(data, f, indent = 2)
+        print(f'The data was saved to folder existing at {json_file} .')
+    
 
-for i in range(1000):
-    passkey_to_test = None
-    result = None
-    passkey_to_test = create_password()
-    result = Strength_Checker.main(passkey_to_test)
-    data.append({'Password': passkey_to_test, "Score": result})
-
-with open(json_file, 'w') as f:
-    json.dump(data, f, indent = 2)
-    print(f'The data was saved to folder existing at {json_file} .')
+if __name__ == '__main__':
+    main()
+else:
+    pass
